a2463fdf233c93b51014b51de94f497cea35702b
IIBlackCode <blackcode-@naver.com>
2023-07-31 12:53:56 +0900

    update home
    update template engine
     - JSP > thymeleaf

--- demo-jar/src/main/java/com/example/demo/HomeController.java
+++ demo-jar/src/main/java/com/example/demo/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 	@GetMapping("/")
 	public String home() {
 		System.out.println("Start Spring Boot : /");
-		return "index";
+		return "thymeleaf/index.html";
 	}
 	
 }
